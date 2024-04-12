380099448c8e481926183e68b6085b65e6723ecb
BKServicesBK <brice.kamganegue@gmail.com>
2024-04-12 23:04:46 +0200

    login comp

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent implements OnInit {
+  form!: FormGroup;
+  loading = false;
+  submitted = false;
+
+  constructor(
+      private formBuilder: FormBuilder,
+      private route: ActivatedRoute,
+      private router: Router
+  ) { }
+
+  ngOnInit() {
+      this.form = this.formBuilder.group({
+          username: ['', Validators.required],
+          password: ['', Validators.required]
+      });
+  }
+
+  get f() { return this.form.controls; }
+
+  onSubmit() {
+    console.log("Submit OK!");
+  }
+}
